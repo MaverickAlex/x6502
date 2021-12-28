2a9343c8f249098b98f48ba619788323f1fa7c4e
MaverickAlex <you@example.com>
2021-12-28 12:45:33 -0600

    added more io address

--- samples/common/io.asm
+++ samples/common/io.asm
@@ -1,4 +1,21 @@
-PORTB             = $6000
-PORTA             = $6001
-DDRB              = $6002
-DDRA              = $6003
+PORTB             = $6000 ;0x6000 = I/O Register B
+PORTA             = $6001 ;0x6001 = I/O Register A
+DDRB              = $6002 ;0x6002 = Data Direction Register B
+DDRA              = $6003 ;0x6003 = Data Direction Register A
+                          ;0x6004 = T1 Low Order Latches/Counter
+                          ;0x6005 = T1 High Order Counter
+                          ;0x6006 = T1 Low Order Latches
+                          ;0x6007 = T1 High Order Latches
+                          ;0x6008 = T2 Low Order Latches/Counter
+                          ;0x6009 = T2 High Order Counter
+SHIFTREG          = $600a ;0x600a = Shift Register
+                          ;0x600b = Auxiliary Control Register
+                          ;0x600c = Peripheral Control Register
+                          ;0x600d = Interrupt Flag Register
+                          ;0x600e = Interrupt Enable Register
+                          ;0x600f = I/O Register A sans Handshake (I do not believe this computer uses Handshake anyway.)
+
+
+
+
+
