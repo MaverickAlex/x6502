f49c2f80de0323ce088032c3364e043cc7347eb1
MaverickAlex <you@example.com>
2021-12-29 09:20:47 -0600

    lcd module has screen update sub

--- samples/common/lcd.asm
+++ samples/common/lcd.asm
@@ -1,3 +1,7 @@
+;this module uses 32 bytes of zero page to allow for faster drawing of lcd.
+LCD_LINE  = $e0   ; $00e0 -> $00ff  stack starts in $0100  
+  
+  
   .include "io.asm"
 
 E  =  %10000000                    ;E
@@ -5,6 +9,13 @@ RW =  %01000000                    ;RW
 RS =  %00100000                    ;RS
 
 init_lcd:
+  lda #" "
+  ldx #32
+zero_LCD_LINE:
+  sta LCD_LINE,x
+  dex 
+  bne zero_LCD_LINE
+  sta LCD_LINE,x
   lda #%11111111 ; Set all pins on port B to output
   sta DDRB
   lda #%11100000 ; Set top 3 pins on port A to output
@@ -12,8 +23,6 @@ init_lcd:
   lda lcd_commands ; Set 8-bit mode; 2-line display; 5x8 font
   jsr lcd_instruction
   lda lcd_commands + 1 ; Display on; cursor on; blink off
-  jsr lcd_instruction
-  lda lcd_commands + 4 ; Increment and shift cursor; don't shift display
   jsr lcd_instruction
   lda lcd_commands + 5 ; Clear display
   jsr lcd_instruction
@@ -49,6 +58,41 @@ lcd_instruction:
   sta PORTA
   pla
   rts
+
+
+
+lcd_updateScreen:
+  jsr lcd_returnHome
+  ldx #0
+lcd_updateScreenFirst16:
+  lda LCD_LINE,x
+  jsr print_char
+  inx
+  cpx #16
+  bne lcd_updateScreenFirst16
+  ldx #0
+lcd_updateScreen_blanks
+  lda lcd_commands + 4
+  jsr lcd_instruction
+  inx
+  cpx #24
+  bne lcd_updateScreen_blanks
+  ldx #16
+lcd_updateScreenLast16
+  lda LCD_LINE,x
+  jsr print_char
+  inx
+  cpx #32
+  bne lcd_updateScreenLast16
+  rts
+
+
+lcd_returnHome:
+  lda lcd_commands + 6
+  jsr lcd_instruction
+  rts
+
+
 
 print_char:     ; prints character on a register
   pha
@@ -89,7 +133,8 @@ lcd_commands:
   byte %00001110 ;1 Display on; cursor on; blink off
   byte %00111100 ;2
   byte %00001100 ;3
-  byte %00000110 ;4 Increment and shift cursor; don't shift display
+  byte %00010100 ;4 shift cursor right
   byte %00000001 ;5 clear 
   byte %00000010 ;6 return home 
   byte %00000000 ;7 
+  byte %00010000 ;8 shift cursor right
